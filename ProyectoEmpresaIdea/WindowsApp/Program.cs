3d3ef8156d27b3792d10278de9e18b89174bc2db
Eduardo <Eduardo@EDUARDO-PC>
2017-02-08 00:27:29 -0600

    se etiqueto la ventana de ejecución de Registro

--- ProyectoEmpresaIdea/WindowsApp/Program.cs
+++ ProyectoEmpresaIdea/WindowsApp/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Registar());
+            //Application.Run(new Registar());
         }
     }
 }
