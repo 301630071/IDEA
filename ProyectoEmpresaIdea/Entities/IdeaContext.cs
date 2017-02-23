278971b3d08810803c5063f5db705601c5802698
Eduardo <Eduardo@EDUARDO-PC>
2017-02-22 19:02:19 -0600

    Se crea clase IdeaContext (cadena de conexión) , tablas enlazadas y metodo que nos permite pluralizar nuestras tablas de la base de datos, Se configura la conexión a la base de Datos

--- ProyectoEmpresaIdea/Entities/IdeaContext.cs
+++ ProyectoEmpresaIdea/Entities/IdeaContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,13 @@ namespace Entities
         {
 
         }
-        public DbSet<Usuario> Usuario { get; set;}
+        public DbSet<Usuario> Usuarios { get; set;}
+        public DbSet<Campus> Campus { get; set; }
+        public DbSet<Registro> Registros { get; set; }
+        public DbSet<Carrera> Carreraas { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
