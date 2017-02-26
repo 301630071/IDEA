1ec101ea37c41dd0d0da1b1d676e92806711abbd
Roy <roge_@DESKTOP-JGF3TDR>
2017-02-25 21:37:07 -0600

    Se Agrego MenuStrip Rgistrar,Buscar

--- ProyectoEmpresaIdea/WindowsApp/VentanAdmin.Designer.cs
+++ ProyectoEmpresaIdea/WindowsApp/VentanAdmin.Designer.cs
@@ -28,21 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.registrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.catalogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alumnosRegistradosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entradaYSalidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoAlumnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entradaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrarToolStripMenuItem,
+            this.catalogoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(498, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // registrarToolStripMenuItem
+            // 
+            this.registrarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoAlumnoToolStripMenuItem,
+            this.entradaToolStripMenuItem,
+            this.salidaToolStripMenuItem});
+            this.registrarToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registrarToolStripMenuItem.Name = "registrarToolStripMenuItem";
+            this.registrarToolStripMenuItem.Size = new System.Drawing.Size(73, 21);
+            this.registrarToolStripMenuItem.Text = "Registrar";
+            // 
+            // catalogoToolStripMenuItem
+            // 
+            this.catalogoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alumnosRegistradosToolStripMenuItem,
+            this.entradaYSalidaToolStripMenuItem});
+            this.catalogoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.catalogoToolStripMenuItem.Name = "catalogoToolStripMenuItem";
+            this.catalogoToolStripMenuItem.Size = new System.Drawing.Size(58, 21);
+            this.catalogoToolStripMenuItem.Text = "Buscar";
+            // 
+            // alumnosRegistradosToolStripMenuItem
+            // 
+            this.alumnosRegistradosToolStripMenuItem.Name = "alumnosRegistradosToolStripMenuItem";
+            this.alumnosRegistradosToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.alumnosRegistradosToolStripMenuItem.Text = "Alumnos Registrados";
+            // 
+            // entradaYSalidaToolStripMenuItem
+            // 
+            this.entradaYSalidaToolStripMenuItem.Name = "entradaYSalidaToolStripMenuItem";
+            this.entradaYSalidaToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.entradaYSalidaToolStripMenuItem.Text = "Entrada y Salida";
+            // 
+            // nuevoAlumnoToolStripMenuItem
+            // 
+            this.nuevoAlumnoToolStripMenuItem.Name = "nuevoAlumnoToolStripMenuItem";
+            this.nuevoAlumnoToolStripMenuItem.Size = new System.Drawing.Size(177, 24);
+            this.nuevoAlumnoToolStripMenuItem.Text = "Nuevo Alumno";
+            // 
+            // entradaToolStripMenuItem
+            // 
+            this.entradaToolStripMenuItem.Name = "entradaToolStripMenuItem";
+            this.entradaToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.entradaToolStripMenuItem.Text = "Entrada";
+            // 
+            // salidaToolStripMenuItem
+            // 
+            this.salidaToolStripMenuItem.Name = "salidaToolStripMenuItem";
+            this.salidaToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.salidaToolStripMenuItem.Text = "Salida";
             // 
             // VentanAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(498, 414);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "VentanAdmin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Administrador";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.VentanAdmin_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem registrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoAlumnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem catalogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alumnosRegistradosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entradaYSalidaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entradaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salidaToolStripMenuItem;
     }
 }
--- ProyectoEmpresaIdea/WindowsApp/VentanAdmin.cs
+++ ProyectoEmpresaIdea/WindowsApp/VentanAdmin.cs
@@ -16,5 +16,10 @@ namespace WindowsApp
         {
             InitializeComponent();
         }
+
+        private void VentanAdmin_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
