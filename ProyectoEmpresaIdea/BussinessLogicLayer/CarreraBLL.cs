d5d9eb3124f07d6bfaaec00862574263eff0f4f1
Eduardo <Eduardo@EDUARDO-PC>
2017-03-17 20:06:27 -0600

    Se borro todo lo que hice

---
+++ ProyectoEmpresaIdea/BussinessLogicLayer/CarreraBLL.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogicLayer
+{
+    public class CarreraBLL
+    {
+      //  public static List<Carrera> ObtenerCarrera()
+        //{
+            //return DataAcessLayer.CarreraDAL.ObtenerCarrera();
+        //}
+    }
+}
